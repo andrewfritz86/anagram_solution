21972777dce73e5cd339deeca8809d8fababb527
andrewfritz86 <andrew.fritz86@gmail.com>
2014-12-17 15:59:29 -0500

    yes

--- lib/connection.rb
+++ lib/connection.rb
@@ -1,10 +1,15 @@
 require 'active_record'
 
-ActiveRecord::Base.establish_connection({
-  :adapter => "postgresql",
-  :host => "localhost",
-  :username => "andrewfritz",#changethis
-  :database => "wordplay"
-  })
+# ActiveRecord::Base.establish_connection
+
+ActiveRecord::Base.establish_connection('postgresql://'+ ENV["DB_INFO"] +'@127.0.0.1/wordplay')
+
+
+# ({
+#   :adapter => "postgresql",
+#   :host => "localhost",
+#   :username => "andrewfritz",#changethis
+#   :database => "wordplay"
+#   })
 
   ActiveRecord::Base.logger = Logger.new(STDOUT)  
